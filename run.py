a07b3ee10472d7e1e78e69ee7dbe9ea49925164c
‘Xuenew;git config --global user.email 18188108851@163.com’ <18188108851@163.com>
2022-08-21 00:25:03 +0800

    # 2022 08 21 周日 1，提交一个测试的接口 ✅

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 # 创建应用实例
 import sys
+import time
 
 from wxcloudrun import app
+# xyy测试热更新
+@app.route('/api/test', methods=['get', 'post'])
+def wxgzh_zzdpz_project_test():
+        return "薛忆阳：》》》》"+str(time.time()) #
 
 # 启动Flask Web服务
 if __name__ == '__main__':
